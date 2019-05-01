48f2552414d084e02835ea7e314661159a84ea7c
firyang <firyang@aliyun.com>
2019-05-01 22:29:11 +0800

    add webpack config

--- node.md
+++ node.md
@@ -1,15 +1,16 @@
-# webpack安装
+## webpack安装
 - 安装本地的webpack
 - webpack webpack-cli -d
 
-# webpack可以进行0配置
+## webpack可以进行0配置
 - 打包工具 -> 输出后的结果(js模块)
 - 打包(支持我们的js的模块化)
 
-# 手动配置webpack
+## 手动配置webpack
 - 默认配置文件的名字 webpack.config.js
 - 指定配置文件： npx webpack --config filename
 - npm run build -- --config webpack.config.js
+
 
 # 引入第三方模块
 ## loader类型
@@ -18,16 +19,16 @@
 - normal 普通loader
 - 后置 postloader
 
-## expose-loader 将模块暴露到全局  
+## 一. expose-loader 将模块暴露到全局  
 `import $ from 'expose-loader?$!jquery'`
 
-## webpack.config.js中配置rules  
+## 二. webpack.config.js中配置rules  
 `{`  
 `test: require.resolve('jquery')`  
 `use: 'expose-loader?$'`  
 `}`
 
-## 使用webpack插件 - webpack.ProvidePlugin, 在每个模块中注入  
+## 三. 使用webpack插件 - webpack.ProvidePlugin, 在每个模块中注入  
 - 引入webpack  
 `const webpack = require('webpack')` 
 - plugins中配置  
@@ -48,12 +49,12 @@
 `document.body.appendChild(image)`  
 - 在css中引入图片
 - `<img src="">`
-## file-loader 默认会在内部生成一张图片到build目录下，把生成的图片名字返回
+## 一. file-loader 默认会在内部生成一张图片到build目录下，把生成的图片名字返回
 `{`  
   `test: /\.(jpg|png|gif)$/,`  
   `use: 'file-loader'`  
 `}`
-## url-loader 可限制图片小于多少k时，用base64转化。否则用file-loader产生真实图片
+## 二. url-loader 可限制图片小于多少k时，用base64转化。否则用file-loader产生真实图片
 `{`  
   `test: /\.(jpg|png|gif)$/,`  
   `use: {`  
@@ -61,8 +62,32 @@
     `options: {`  
       `limit: 10 * 1024`  
 `}}}`
-## html-withimg-loader 处理<img src="">
+## 三. html-withimg-loader 处理`<img src="">`
 `{`  
   `test: /\.html$/,`  
   `use: 'html-withimg-loader'`  
+`}` 
+
+# source map
+## devtool 增加映射文件，可以帮我们调试源代码
+- 1. 源码映射 会单独生成一个sourcemap文件 出错了 会标识  当前报错的列和行  
+`devtool： 'source-map'`  
+- 2. 不会产生单独文件，但是可以显示行和列   
+`devtool： 'eval-source-map'`  
+- 3. 不会产生列，但是是一个单独的映射文件  
+`devtool： 'cheap-module-source-map'`  
+- 4. 不会产生文件，集成在打包后的文件中，不会产生列  
+`devtool： 'cheap-module-eval-source-map'` 
+
+# watch 实时打包
+## 配置 watch
+- `watch: true`  
+## 配置 watchOptions
+- poll 每秒问我多少次
+- aggregateTimeout 防抖 停止输入500ms后再打包
+- ignored 配置不需要监控的文件夹   
+`watchOptions: {`  
+  `poll: 1000,`  
+  `aggregateTimeout: 500,`  
+  `ignored: /node_modules/`  
 `}`  
--- webpack.config.js
+++ webpack.config.js
@@ -1,189 +1,32 @@
+/**
+ * 打包多页应用
+ * 多个入口文件
+ * 对应生成多个html和js
+ */
+
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin') // 将样式放入css文件中
-const OptimizeCss = require('optimize-css-assets-webpack-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const webpack = require('webpack')
 
 module.exports = {
-  optimization: { // 优化项
-    minimizer: [
-      new UglifyJsPlugin({ // 压缩js
-        cache: true,
-        parallel: true,
-        sourceMap: true
-      }),
-      new OptimizeCss()  // 压缩css
-    ]
-  },
-  devServer: { // 开发服务器
-    port: 3000,
-    contentBase: './dist',
-    // historyApiFallback: { // 返回404页面时定向到特定页面
-    //   rewrites:[
-    //     {from:/./, to:'/404.html'}
-    //   ]
-    // }, 
-    hot: true,
-    inline: true,
-    progress: false,
-    compress: true,
-    overlay: { // 在浏览器输出编译错误
-      warnings: true,
-      errors: true
-    },
-    stats: 'errors-only', // 控制编译的时候shell上的输出内容
-    open: false,
-    proxy:{}
-  },
-  mode: 'development', // 模式 默认两种 production development
-  entry: './src/index.js', // 入口
+  mode: 'development',
+  entry: {
+    home:'./src/home.js', 
+    other:'./src/other.js'
+  }, 
   output: {
-    filename: 'bundle.js', // 打包后的文件名
-    // filename: 'bundle.[hash:8].js', // 打包后的文件名
-    path: path.resolve(__dirname, 'dist') // 路径必须是一个绝对路径
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
   },
-  plugins: [ // 数组 放着所有的webpack插件
+  plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      filename: 'index.html',
-      minify: {
-        removeAttributeQuotes: true, // 清除双引号
-        collapseWhitespace: true // 清除断行
-      },
-      hash: true
+      filename: 'home.html',
+      chunks: ['home']
     }),
-    new MiniCssExtractPlugin({
-      filename: 'main.css'
-    }),
-    new webpack.ProvidePlugin({ // 在每个模块中注入第三方模块
-      $: 'jquery'
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'other.html',
+      chunks: ['other']
     })
-  ],
-  externals: { // 打包时忽略第三方模块 
-    // jquery: "jQuery"
-  },
-  module: { // 模块
-    rules: [ // 规则
-      // loader的特点 希望单一
-      // loader的用户 字符串只用一个loader
-      // 多个loader需要 []
-      // loader的顺序 默认是从右向左, 从下到上执行
-      // 可写成对象
-
-      //---------------------------
-      // 引入第三方模块
-      // {
-      //   test: require.resolve('jquery'),
-      //   use: 'expose-loader?$'
-      // },
-
-      //---------------------------
-      // 代码校验
-      // 安装 eslint eslint-loader
-      {
-        // enforce: "pre",
-        // test: /\.js$/,
-        // exclude: /node_modules/,
-        // loader: "eslint-loader"
-      },
-
-      //---------------------------
-      // 处理js, es6,7 -> es5
-      // babel-loader
-      // @babel/preset-env es6 -> es5
-      // @babel/plugin-proposal-decorators 类装饰器 -> es5
-      // @babel/plugin-proposal-class-properties 类属性 -> es5
-      // @babel/plugin-transform-runtime 将高级实例方法转换es5
-      // @babel/runtime --save
-      // @babel/polyfill --save 将高级实例方法转换polyfill 安装后在脚本中引入require("@babel/polyfill")
-      {
-        test: /\.js$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env'
-            ],
-            plugins: [
-              ["@babel/plugin-proposal-decorators", { "legacy": true }],
-              ["@babel/plugin-proposal-class-properties", { "loose": true }],
-              "@babel/plugin-transform-runtime"
-            ]
-          }
-        },
-        include: path.resolve(__dirname, 'src'),
-        exclude: /node_modules/
-      },
-
-      // ---------------------------
-      // 处理css
-      // css-loader 解析@import这种语法
-      // style-loader 它是把css 插入到head标签中
-      {
-        test: /\.css$/,
-        use: [
-          // { // 把css 插入到head标签中
-          //   loader:'style-loader',
-          //   options:{ // 配置项
-          //     // 插入到head顶部
-          //     insertAt: 'top'
-          //   }
-          // },
-          // 把css抽离, 并放入main.css,
-          MiniCssExtractPlugin.loader,
-          // 解析@import路径
-          'css-loader',
-          // 添加浏览器前缀
-          // 1. 安装postcss-loader autoprefixer
-          // 2. 添加postcss.config.js配置文件 
-          'postcss-loader',
-        ]
-      },
-
-      // ---------------------------
-      // 处理less文件 sass stylus
-      { 
-        test: /\.less$/,
-        use: [
-          // {
-          //   loader: 'style-loader',
-          //   options: {
-          //     insertAt: 'top'
-          //   }
-          // },
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          // less -> css
-          'less-loader'
-        ]
-      },
-
-      // ---------------------------
-      // 处理图片
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        use: {
-          // loader: 'url-loader',
-          // options: {
-          //   // 限制图片小于多少k时，用base64转化。否则用file-loader产生真实图片
-          //   limit: 20 * 1024
-            
-          // }
-          loader: 'url-loader',
-          options: {
-            limit: 10 * 1024
-          }
-        }
-      },
-
-      // ---------------------------
-      // 处理 <img src="./logo.png" />
-      {
-        test: /\.html$/,
-        use: 'html-withimg-loader'
-      }
-    ]
-  }
+  ]
 }
